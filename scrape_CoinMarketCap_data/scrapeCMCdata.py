d735fbe0bc30627cd3249ecd187c1b15c12662c4
David C. Milam <36645087+dcm026@users.noreply.github.com>
2020-12-19 11:01:28 -0600

    Modified get_historical_data to reflect changes made on CMC

--- scrape_CoinMarketCap_data/scrapeCMCdata.py
+++ scrape_CoinMarketCap_data/scrapeCMCdata.py
@@ -1,4 +1,3 @@
-
 import time
 import pandas as pd
 from urllib.request import urlopen
@@ -6,6 +5,7 @@ from bs4 import BeautifulSoup
 from scrapy import Selector
 import re
 from time import strptime
+import json
 
 
 def save_csv(text, fileName, folderName='', header=False, write_append='w'):
@@ -22,6 +22,7 @@ def save_csv(text, fileName, folderName='', header=False, write_append='w'):
     df = pd.DataFrame(text)
     df.to_csv(filePath, index=False, header=header, mode=write_append)
 
+
 def get_html_recur(ticker, url, attempt):
     if attempt == 3:
         print('could not get data for', ticker)
@@ -37,63 +38,61 @@ def get_html_recur(ticker, url, attempt):
         print('failed to get data for', ticker, ' trying again')
         return get_html_recur(ticker, url, (attempt + 1))
 
+
 def get_historical_data():
     # load cryptos from csv which we want price data on
-    df_desiredCryptos = pd.read_csv('Crypto List.csv')
+    df_desiredCryptos = pd.read_csv('Crypto List(binance).csv')
     cmcNames = list(df_desiredCryptos['cmcName'])
     tickers = list(df_desiredCryptos['Ticker'])
     startDates = list(df_desiredCryptos['StartDate'])
-    failed = [] #
+    failed = []  #
     mat = [['Ticker', 'Date(YMD)', 'Open', 'High', 'Low', 'Close', 'Vol', 'MktCap']]
 
     for i in range(len(tickers)):
         ticker = tickers[i]
         name = cmcNames[i]
+        if ticker == 'MktCap' or ticker == 'MktVol':
+            continue
         time.sleep(5)
 
         try:
             print('getting data for:', ticker, '  i:', i)
             startDate = '20' + str(startDates[i])
-            endDate = time.strftime('%Y%m%d') # current date in yyyymmdd format as endpoint
+            endDate = time.strftime('%Y%m%d')  # current date in yyyymmdd format as endpoint
             url = 'https://coinmarketcap.com/currencies/' + str(name) + '/historical-data/?start=' + str(startDate) + '&end=' + str(endDate)  # url for getting current price for every coin
+            print(url)
             html = get_html_recur(ticker, url, attempt=0)
             if html == -1:
                 failed.append(ticker)
                 continue
             soup = BeautifulSoup(html, features="html.parser")
-            table = soup.find('tbody') # format changed from table to tbody as of 6/26/2019
-            for tr in table.find_all('tr'):
-                row = [td.text for td in tr.find_all('td')]
-                try:
-                    if len(row) == 0:
-                        continue
-                    date = str(row[0]).replace(',', '').split(' ') # 'Jun 04, 2019' -> ['Jun', '04', '2019']
-                    month = str(strptime(date[0], '%b').tm_mon) # 'Jun' -> '6'
-                    month = '0' + month if len(month) < 2 else month
-                    yyyymmdd = str(date[2]) + month + str(date[1])
-                    vol = int(float(row[5].replace(',', '')))
-                    mktCap = row[6].replace(',', '').replace('-', '-1') # if market cap is blank, insert -1
-                    mktCap = int(float(mktCap))
+            data = soup.find('script', id='__NEXT_DATA__', type='application/json')
+            raw_json = json.loads(data.contents[0])
+            data = raw_json['props']['initialState']['cryptocurrency']['ohlcvHistorical']
+            for key in data.keys():
+                for row in data[key]['quotes']:
+                    row = row['quote']['USD']
+                    # print(row) # {'open': 0.00044700701255351305, 'high': 0.00045809600851498544, 'low': 0.00040902700857259333, 'close': 0.00040902700857259333, 'volume': 260.7489929199219, 'market_cap': 175370.933835, 'timestamp': '2016-01-02T23:59:59.999Z'}}
+                    yyyymmdd = str(row['timestamp'].replace('-', '').split('T')[0]) # '2016-01-02T23:59:59.999Z' -> '20160102
                     #           ticker, date      open,   high,   low,    close,
-                    mat.append([ticker, yyyymmdd, row[1], row[2], row[3], row[4], vol, mktCap])
-                    # print(len(mat), mat)
-                except Exception as e:
-                    print('error @ ', ticker, 'error: ', e)
+                    mat.append([ticker, yyyymmdd, row['open'], row['high'], row['low'], row['close'], row['volume'], row['market_cap']])
         except Exception as e:
             print('error @ ', ticker, 'error: ', e)
             failed.append(ticker)
+
         save_csv(mat, fileName=('historicalData' + str(time.strftime('%Y%m%d'))), folderName='CMCdata', header=False, write_append='a')
         mat = []
         time.sleep(10)
     print('Failed to get data for coins: ', failed)
 
+
 def get_current_day_data():
     print('getting data for current day')
     # load cryptos from csv which we want price data on
-    df_desiredCryptos = pd.read_csv('Crypto List.csv')
+    df_desiredCryptos = pd.read_csv('Crypto List(binance).csv')
     desiredCryptos = list(df_desiredCryptos['Ticker'])
 
-    url = 'https://coinmarketcap.com/all/views/all/' # url for getting current price for every coin
+    url = 'https://coinmarketcap.com/all/views/all/'  # url for getting current price for every coin
     html = get_html_recur('get_current_day_data', url, attempt=0)
     if html == -1:
         return
@@ -103,7 +102,7 @@ def get_current_day_data():
 
     sel = Selector(text=soup.prettify())
     coins = sel.xpath("//tr[contains(@id, 'id-')]").extract()
-    date = time.strftime('%Y%m%d') # current date in yyyymmdd format
+    date = time.strftime('%Y%m%d')  # current date in yyyymmdd format
 
     for coin in coins:
         try:
@@ -111,7 +110,7 @@ def get_current_day_data():
             sel = Selector(text=soup.prettify())
 
             ticker = sel.xpath("//td[contains(@class, 'col-symbol')]/text()").extract_first()
-            ticker = re.sub(r'\W', '', ticker) # remove all spaces, newlines, & tabs
+            ticker = re.sub(r'\W', '', ticker)  # remove all spaces, newlines, & tabs
             if ticker not in desiredCryptos and ticker != 'BTC':
                 continue
             price = sel.xpath("//a[@class='price']/@data-usd").extract_first()
@@ -126,10 +125,10 @@ def get_current_day_data():
             print(e)
     save_csv(mat, fileName=str(time.strftime("%Y%m%d%H%M%S")), folderName='CMCdata')
 
+
 if __name__ == '__main__':
     '''Get price data for this current moment'''
-    get_current_day_data()
+    # get_current_day_data()
 
     '''Get historical price data'''
     get_historical_data()
-
